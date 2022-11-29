6fd5e6be18baae7d4ba227984bdbf64442ca079b
Prince <princerdhameliya1603@gmail.com>
2022-11-29 19:26:32 +0530

    dfdf

--- server.js
+++ server.js
@@ -31,8 +31,8 @@ mongoose.connect(process.env.MONGO_DB,
 .catch((error)=>console.log("Error while connecting with the database", error));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('client/build'))
-    app.get("*", (req, res) => {
+    app.use(express.static(path.join(__dirname+'./client/build')))
+    app.get("/*", (req, res) => {
         res.sendFile(path.join(__dirname+'./client/build/index.html'));
     });
 }
