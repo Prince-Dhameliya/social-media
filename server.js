8e4c286fb591980c625b3a7e42b3d51205a7a376
Prince <princerdhameliya1603@gmail.com>
2022-11-29 16:54:32 +0530

    chang

--- server.js
+++ server.js
@@ -30,9 +30,6 @@ mongoose.connect(process.env.MONGO_DB,
 
 if(process.env.NODE_ENV === "production"){
     app.use(express.static('client/build'))
-    app.get("/*", function(req, res) {
-    res.sendFile(path.join(__dirname, "./client/build/index.html"));
-    });
 }
 
 // usage of routes
