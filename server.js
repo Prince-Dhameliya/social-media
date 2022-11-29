584d38d694eecf5494690f905f47e2fb9debadb9
Prince <princerdhameliya1603@gmail.com>
2022-11-29 20:42:38 +0530

    jdfkjdsksfjjkfd

--- server.js
+++ server.js
@@ -28,11 +28,11 @@ mongoose.connect(process.env.MONGO_DB,
 .then(()=>app.listen(PORT, ()=>console.log(`listening at ${PORT}`)))
 .catch((error)=>console.log("Error while connecting with the database", error));
 
-if(process.env.NODE_ENV === "production"){
+if(process.env.NODE_ENV == "production"){
     app.use(express.static('client/build'))
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-    });
+    app.get("*",(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+    })
 }
 
 // usage of routes
