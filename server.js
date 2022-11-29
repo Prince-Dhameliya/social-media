8b13ca42dc2fa99a28d0f8b786f5d3833df085c1
Prince <princerdhameliya1603@gmail.com>
2022-11-29 18:42:15 +0530

    ccc

--- server.js
+++ server.js
@@ -29,11 +29,12 @@ mongoose.connect(process.env.MONGO_DB,
 .catch((error)=>console.log("Error while connecting with the database", error));
 
 if(process.env.NODE_ENV === "production"){
-    app.use(express.static('build'))
+    app.use(express.static('client/build'))
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+        res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
     });
 }
+
 
 // usage of routes
 app.use('/auth', AuthRoute)
