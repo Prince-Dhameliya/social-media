aca4efe30c634fa7f11ad6511da1090c239c0985
Prince <princerdhameliya1603@gmail.com>
2022-12-13 14:36:46 +0530

    first commit

--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({baseURL: "http://localhost:5000"})
+const API = axios.create({baseURL: ""})
 
 export const uploadImage = (data) => API.post('/upload', data)
 export const uploadPost = (data) => API.post("/posts", data)
