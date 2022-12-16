eedf216b189e5ad9740dadd8afcc19d624609919
Prince <princerdhameliya1603@gmail.com>
2022-12-16 22:53:09 +0530

    main

--- client/src/Components/Posts/Posts.jsx
+++ client/src/Components/Posts/Posts.jsx
@@ -38,9 +38,9 @@ const Posts = () => {
       </div>
 
       <div className='NavigationBar'>
-            <Link to="../home"><img src={Home} alt="" style={{width: "1.6rem", height: "1.6rem"}} /></Link>
-            <Link to=""><img src={WAddObj} alt="" style={{width: "2.2rem", height: "2.2rem"}} /></Link>
-            <Link to={`../Profile/${user._id}`}><img src={user.profilePicture} alt="" style={{width: "2rem", height: "2rem"}} /></Link>
+            <Link to="../home"><img src={Home} alt="" className='homeNavigation' /></Link>
+            <Link to=""><img src={WAddObj} alt="" className='addPostNavigation' /></Link>
+            <Link to={`../Profile/${user._id}`}><img src={user.profilePicture} className="profileNavigation" alt="" /></Link>
       </div>
     </>
   )
