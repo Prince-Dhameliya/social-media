797b0aebbc728e77ac6b50771c75b6157d398395
Prince <princerdhameliya1603@gmail.com>
2023-01-31 16:57:30 +0530

    new

--- socket/index.js
+++ socket/index.js
@@ -1,11 +1,13 @@
 import {Server} from "socket.io";
+import dotenv from "dotenv";
 
 dotenv.config()
 const PORT = process.env.PORT || 7000;
 
 const io = new Server(PORT,{
     cors:{
-        origin:"http://social-point-23.vercel.app/",
+        origin:"http://social-point-24.vercel.app",
+        credentials: true
     }
 });
 
