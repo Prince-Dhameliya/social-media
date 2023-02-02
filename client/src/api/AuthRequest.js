78bc877145fa82914b0019d1ba5e618ac8885db1
Prince <princerdhameliya1603@gmail.com>
2023-02-02 18:36:44 +0530

    new

--- client/src/api/AuthRequest.js
+++ client/src/api/AuthRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({baseURL: "http://localhost:5000/"})
+const API = axios.create({baseURL: "/"})
 
 export const logIn = (formData) => API.post('/api/auth/login', formData)
 export const signUp = (formData) => API.post('/api/auth/register', formData)
--- client/src/api/UploadRequest.js
+++ client/src/api/UploadRequest.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-const API = axios.create({baseURL: "http://localhost:5000/"})
+const API = axios.create({baseURL: "/"})
 
 export const uploadImage = (data) => API.post('/api/upload', data)
 export const uploadPost = (data) => API.post("/api/posts", data)
