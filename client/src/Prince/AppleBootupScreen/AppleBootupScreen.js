98e3f95757b2adc27054977ebc4fe68985d5dee1
Prince <princerdhameliya1603@gmail.com>
2023-02-21 01:31:35 +0530

    new

--- client/src/Prince/AppleBootupScreen/AppleBootupScreen.js
+++ client/src/Prince/AppleBootupScreen/AppleBootupScreen.js
@@ -6,7 +6,6 @@ const AppleBootupScreen = () => {
   return (
     <div className='AppleBootupScreen' id='AppleBootupScreen'>
         <img src={Logo} alt="" className='logo' />
-        {/* <i className='fa fa-apple logo'></i> */}
         <div className="progress"></div>
     </div>
   )
